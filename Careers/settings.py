1a89b8404474c02c77afedf0375252f6631299ee
AndryRana <lsourcus@gmail.com>
2023-04-04 00:34:05 +0200

    DB comment

--- Careers/settings.py
+++ Careers/settings.py
@@ -83,9 +83,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         # "NAME": config('DB_NAME'),
-        "USER": config('DB_USER'),
-        "PASSWORD": config('DB_USER_PASSWORD'),
-        "HOST": config('DB_HOST'),
+        # "USER": config('DB_USER'),
+        # "PASSWORD": config('DB_USER_PASSWORD'),
+        # "HOST": config('DB_HOST'),
         
     }
 }
